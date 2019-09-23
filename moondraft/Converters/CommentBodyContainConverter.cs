00d6df2e5b0a75949a08cb2ffcda1c38693249d7
caramelopardalis <ltsu.koiwai@gmail.com>
2019-09-23 22:18:42 +0900

    Improve design

---
+++ moondraft/Converters/CommentBodyContainConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace moondraft.Converters
+{
+    public class CommentBodyContainConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value as string)?.Length > 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
